b648b742368471d60b90b7a86e2539892764d8fb
ChanwitW <witaazaza245@gmail.com>
2024-11-25 14:32:03 +0700

    Lab101.java

--- LabSheet/LabSheet01/src/Lab101.java
+++ LabSheet/LabSheet01/src/Lab101.java
@@ -17,8 +17,15 @@ public class Lab101 {
 		System.out.println("Total Price is : "+ totalPrice+" baht. ");
 		System.out.println("-------------------------------------");
 		
-		System.out.print("How many Discount : ");
+		System.out.print("How many Discount (%) : ");
 		int Vat = scan.nextInt(); 
+		System.out.println("-------------------------------------");
+		
+		float discount = totalPrice*Vat/100;
+		float paid = totalPrice-discount;
+		System.out.println("Discount from "+Vat+ "%" +" "+discount+"baht.");
+		System.out.println("Amount to be paid "+""+""+paid+"baht.");
+		
 
 	}
 
