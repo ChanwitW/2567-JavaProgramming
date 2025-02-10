2acd860ba263e9c40cffd03e4c452c353dfd2ad8
ChanwitW <witaazaza245@gmail.com>
2025-02-10 14:11:06 +0700

    10/2/68

---
+++ Labsheet09/src/Movie.java
@@ -0,0 +1,15 @@
+
+public class Movie {
+	private String id;
+	private String name;
+	private director director;
+	
+	
+	public Movie(String name , String id , director director) {
+		this.name = name;
+		this.id = id;
+		this.director = director;
+	}
+	
+	
+}
---
+++ Labsheet09/src/director.java
@@ -0,0 +1,40 @@
+
+public class director {
+	 private String name;
+	 private String email;
+	 private char gender;
+	 
+	 public director(String name , String email , char gender) {
+		 this.name = name;
+		 this.email = email;
+		 this.gender = gender;
+	 }
+	 
+	 public director(String name , String email) {
+		 this(name, email, '0');
+	 }
+	 
+	 public String getName() {
+		return this.name;
+	}
+	 
+	 public String getEmail() {
+		return this.email;
+	}
+	 
+	 public String getGnderName() {
+		if (gender == 'm' || gender == 'M') {
+			return "Male";
+		}
+		else if (gender == 'f' || gender == 'F') {
+			return "Female";
+		}
+		else {
+			return null;
+		}
+	}
+	 
+	public String toString() {
+		return name + "(" + email + getGnderName() + ")";
+	}
+}
